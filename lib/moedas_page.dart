908bbb83f19b162f03c287b65aea77712a858934
AllanGd <allan_gd@hotmail.com>
2023-07-03 23:35:14 -0300

    AppBar cunstonization

--- lib/moedas_page.dart
+++ lib/moedas_page.dart
@@ -12,50 +12,82 @@ class MoedasPage extends StatefulWidget {
 }
 
 class _MoedasPageState extends State<MoedasPage> {
-  final tabela = MoedaRepository.tabela;
+  final moedaRepository = MoedaRepository.tabela;
 
   NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');
 
   List<Moeda> selecionadas = [];
+
+  appBarDinamica() {
+    if (selecionadas.isEmpty) {
+      return AppBar(
+        title: const Text('Cripto Moedas'),
+      );
+    } else {
+      return AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            setState(() {
+              selecionadas = [];
+            });
+          },
+        ),
+        title: Text('${selecionadas.length} selecionadas'),
+        backgroundColor: Colors.blueGrey[50],
+        iconTheme: const IconThemeData(color: Colors.black87),
+        titleTextStyle: const TextStyle(
+            color: Colors.black87, fontSize: 20, fontWeight: FontWeight.bold),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Cripto Moedas'),
-      ),
+      appBar: appBarDinamica(),
       body: ListView.separated(
           itemBuilder: (BuildContext context, int moeda) {
             return ListTile(
               shape: const RoundedRectangleBorder(
                   borderRadius: BorderRadius.all(Radius.circular(12))),
-              leading: (selecionadas.contains(tabela[moeda]))
+              leading: (selecionadas.contains(moedaRepository[moeda]))
                   ? const CircleAvatar(
                       child: Icon(Icons.check),
                     )
                   : SizedBox(
                       width: 40,
-                      child: Image.asset(tabela[moeda].icone),
+                      child: Image.asset(moedaRepository[moeda].icone),
                     ),
               title: Text(
-                tabela[moeda].nome,
+                moedaRepository[moeda].nome,
                 style:
                     const TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
               ),
-              trailing: Text(real.format(tabela[moeda].preco)),
+              trailing: Text(real.format(moedaRepository[moeda].preco)),
               selectedTileColor: Colors.indigo[50],
-              selected: selecionadas.contains(tabela[moeda]),
+              selected: selecionadas.contains(moedaRepository[moeda]),
               onLongPress: () {
                 setState(() {
-                  (selecionadas.contains(tabela[moeda]))
-                      ? selecionadas.remove(tabela[moeda])
-                      : selecionadas.add(tabela[moeda]);
+                  (selecionadas.contains(moedaRepository[moeda]))
+                      ? selecionadas.remove(moedaRepository[moeda])
+                      : selecionadas.add(moedaRepository[moeda]);
                 });
               },
             );
           },
           padding: const EdgeInsets.all(16),
           separatorBuilder: (_, __) => const Divider(),
-          itemCount: tabela.length),
+          itemCount: moedaRepository.length),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: selecionadas.isNotEmpty ? FloatingActionButton.extended(
+        onPressed: () {},
+        label: const Text(
+          "Favoritar",
+          style: TextStyle(letterSpacing: 0, fontWeight: FontWeight.bold),
+        ),
+        icon: const Icon(Icons.star),
+      ) : null,
     );
   }
 }
